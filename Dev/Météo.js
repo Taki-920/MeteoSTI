105cf8ae829bdcf842bbf9ac11bfb54c63337dfd
Taki-920 <nael1804@outlook.fr>
2025-11-20 17:45:15 +0100

    enlever l'alert

--- Dev/Météo.js
+++ Dev/Météo.js
@@ -1,8 +1,9 @@
 const url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
-alert("erreur")
+
 fetch(url)
     .then(reponse => reponse.json())
     .then(data => {
         console.log("Données météo :");
         console.log(data);
+
     })
