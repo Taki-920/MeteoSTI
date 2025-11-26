68bc7ccf5a61035f911edffc631b5a179d090d6f
Taki-920 <nael1804@outlook.fr>
2025-11-26 23:51:26 +0100

    geo loc + bouton get

--- Dev/Météo.js
+++ Dev/Météo.js
@@ -21,21 +21,51 @@ fetch(url)
  const IndiceUvEl = document.getElementById('UV')
  const AléasclimatiquealertesimplesEl = document.getElementById('Risques');
  const conditionEl = document.getElementById('condition');
+
+ document.getElementById('boutonGet').addEventListener('click,'() => {
+    const localisation = document.getElementById('LocalisationInput').valeur.trim();
+    if (localisation) { setstatus('Entrez une ville, lat ou lon'); return; }
+     if (localisation.includes(',')) {
+    const [lat,lon] = v.split(',').map(s => s.trim());
+    if (!isFinite(lat) || !isFinite(lon)) { setstatus('Format lat,lon incorrect'); return; }
+    getWeather(Number(lat), Number(lon), 'Coordonnées saisies');
+  } else {
+    geocodeCity(localisation);
+  }
+});
+document.getElementById('btnGeo').addEventListener('click', () => {
+  if (!navigator.geolocation) { setstatus('Géolocalisation non prise en compte'); return; }
+  setstatus('Récupération position…');
+  navigator.geolocation.getCurrentPosition(p => {
+    getWeather(p.coords.latitude, p.coords.longitude, 'Position actuelle');
+  }, e => {
+    setstatus('Impossible dobtenir la position: ' + e.message);
+  });
+});
  
  function weatherCodeToText(code) {
   const map = {
-    0: "Ciel dégagé",
-    1: "Ensoleillé",
+    0: "Ciel dégagé / ensoleillé",
+    1: "Principalement ensoleillé",
     2: "Partiellement nuageux",
-    3: "Très nuageux",
+    3: "Couvert / nuageux",
     45: "Brouillard",
-    51: "Bruine",
+    48: "Dépôts givre / brouillard",
+    51: "Bruine légère",
+    53: "Bruine modérée",
+    55: "Bruine dense",
     61: "Pluie légère",
     63: "Pluie modérée",
     65: "Pluie forte",
     71: "Neige faible",
-    80: "Averses",
-    95: "Orage"
+    73: "Neige modérée",
+    75: "Neige forte",
+    80: "Averses légères",
+    81: "Averses modérées",
+    82: "Averses violentes",
+    95: "Orages",
+    96: "Orages avec grêle légère",
+    99: "Orages violents avec grêle"
   };
-  return map[code] || "Code inconnu";
+  return map[code] || "Code météo inconnu";
 }
